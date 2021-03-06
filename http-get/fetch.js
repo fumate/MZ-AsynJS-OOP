3b1a9eb0a53fa84fef6b9b1afbc143c2b055fe6b
fumate <mate.fuller@gmail.com>
2021-03-06 11:16:11 +0100

    first task completed

--- http-get/fetch.js
+++ http-get/fetch.js
@@ -1,4 +1,13 @@
-//can be deleted
-console.log("Console log works!");
+fetch("https://jsonplaceholder.typicode.com/users")
+    .then(
+        response => {
+            if (!response.ok) {
+                throw Error(response.statusText)
+            } else {
+                return response.json()
+            }
+        })
+        .then(data => console.log(data))
+        .catch(error => console.error("Could not fetch data", error));
 
 
